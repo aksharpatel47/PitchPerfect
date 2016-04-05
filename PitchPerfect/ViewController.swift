bf6b77a88efc851801c9068051367d78e1d1817d
Akshar Patel <akshar.patel.47@gmail.com>
2016-04-05 09:16:25 +0530

    Added icons for buttons and wired the outlets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
   
   @IBOutlet weak var recordingLabel: UILabel!
+  @IBOutlet weak var stopRecordingButton: UIButton!
+  @IBOutlet weak var recordButton: UIButton!
   
   override func viewDidLoad() {
     super.viewDidLoad()
